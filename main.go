a4ccbceaf586d2001f61b0cf85fed21eaa399ebe
Dmitrii Leshchev <dmchel91@gmail.com>
2026-02-07 15:25:55 +0500

    Metrics handler

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 
 	h "github.com/dmchel/bootdev-chirpy/handlers/healthcheck"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -14,9 +20,36 @@ func main() {
 		Handler: mux,
 	}
 
-	mux.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir("./app"))))
+	apiCfg := apiConfig{}
+	fs := http.FileServer(http.Dir("./app"))
+
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(fs)))
 	mux.HandleFunc("/healthz", h.HealthcheckHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetMetricsHandler)
 
 	log.Println("Starting server", server.Addr)
 	log.Panic(server.ListenAndServe())
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+
+	hits := "Hits: " + strconv.Itoa(int(cfg.fileserverHits.Load()))
+	w.Write([]byte(hits))
+}
+
+func (cfg *apiConfig) resetMetricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Status: OK"))
 }
